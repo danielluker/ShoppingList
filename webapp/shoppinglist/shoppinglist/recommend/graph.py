89144f2c368fb58a085d0491b160c1a1cf0f76ee
Daniel F. Luker <daniel.f.luker@gmail.com>
2015-12-07 17:09:11 -0500

    Adding library which implements a simple graph model which will be used for the recommendations.

---
+++ webapp/shoppinglist/shoppinglist/recommend/graph.py
@@ -0,0 +1,69 @@
+"""
+    Defines a graph data structure to store recommendations
+
+    @author Daniel F. Luker
+    @date 7th December 2015
+"""
+
+import operator
+
+
+class Graph(object):
+    """ Handles all the functionality on the graph """
+    def __init__(self, name="Graph"):
+        self.name = name
+        self.__data = {}
+
+    def add(self, value):
+        """ Inserts a value by creating a new node.
+            If the value is already in the graph, does nothing
+        """
+        if value not in self.__data:
+            new_node = _Node()
+            self.__data[value] = new_node
+
+    def add_relationship(self, value_a, value_b):
+        """ Creates (or strengthens) a relationship between two nodes """
+        if value_a == value_b:
+            return
+        if value_a not in self.__data:
+            self.add(value_a)
+        if value_b not in self.__data:
+            self.add(value_b)
+        self.__data[value_a].add_adjacent(value_b)
+        self.__data[value_b].add_adjacent(value_a)
+
+    def add_relationship_list(self, relationships):
+        """ Relates all objects in the list to each other """
+        for i in range(len(relationships)):
+            for j in range(i, len(relationships)):
+                self.add_relationship(relationships[i], relationships[j])
+
+    def contains(self, value):
+        """ Checks if the value is contained in the graph """
+        return value in self.__data
+
+    def __str__(self):
+        result = ""
+        for value in self.__data:
+            result += value + " -> "
+            result += self.__data[value].get_neighbours().__str__() + "\n"
+        return result
+
+
+class _Node(object):
+    """ Represents a node in a graph """
+    def __init__(self, data=None):
+        self.__adjacent = {}
+        self.__data = data
+
+    def add_adjacent(self, neighbour):
+        """ Adds a relationship to this node """
+        if neighbour in self.__adjacent:
+            self.__adjacent[neighbour] = self.__adjacent[neighbour] + 1
+        else:
+            self.__adjacent[neighbour] = 1
+
+    def get_neighbours(self):
+        """ Fetches the map of (neighbours, weight) sorted by weight """
+        return sorted(self.__adjacent.items(), key=operator.itemgetter(1))
---
+++ webapp/shoppinglist/shoppinglist/recommend/tester.py
@@ -0,0 +1,38 @@
+""" Class to test the graph functionality
+
+    @author Daniel F. Luker
+    @date 7th December 2015
+"""
+
+import graph
+
+
+def test_graph_1():
+    """ testing an interconnected graph """
+    data = ['potatoes', 'milk', 'eggs', 'rice']
+    test_graph = graph.Graph()
+    for value in data:
+        test_graph.add(value)
+    print(test_graph)
+    test_graph.add_relationship('milk', 'eggs')
+    test_graph.add_relationship('eggs', 'rice')
+    test_graph.add_relationship('milk', 'potatoes')
+    test_graph.add_relationship('milk', 'rice')
+    test_graph.add_relationship('milk', 'eggs')
+    print(test_graph)
+
+
+def test_graph_2():
+    data1 = ['potatoes', 'milk', 'eggs', 'rice']
+    data2 = ['milk', 'cereal', 'pizza']
+    data3 = ['beer', 'pizza', 'chips']
+    data4 = ['milk', 'cereal']
+    test_graph = graph.Graph()
+    test_graph.add_relationship_list(data1)
+    test_graph.add_relationship_list(data2)
+    test_graph.add_relationship_list(data3)
+    test_graph.add_relationship_list(data4)
+    print(test_graph)
+
+if __name__ == '__main__':
+    test_graph_2()
